e3a55b494e9f47c5049eedccabc34a51c8f0b949
Fede-Dev <87677596+fedesch26@users.noreply.github.com>
2022-05-05 17:01:24 -0300

    Delete actions directory

--- actions/index.js
+++
@@ -1,3 +0,0 @@
-require('./balance');
-require('./privatekey');
-require('./genqr');
--- actions/main.js
+++
@@ -1,9 +0,0 @@
-const { bot, Composer, database } = require('../core');
-const composer = new Composer();
-const { templates } = require('../libs');
-
-composer.hears(/lbt\s/i, async (ctx, next) => {
-  await next();
-});
-
-bot.use(composer);
